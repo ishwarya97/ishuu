0c95b8b3d96008b1166d25c8ca19e23e83e003cd
ishwarya97 <30829605+ishwarya97@users.noreply.github.com>
2018-01-11 16:46:52 +0530

    Create vowel.c

---
+++ vowel.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char c;
+int is lowercasevowel,i is uppercasevowel;
+printf("it is an alphabet");
+scanf("%c",&c);
+is lowercasevowel(c=='a'||c=='e'||c='i'||c=='o'||c=='u');
+is uppercasevowel(c=='A'||c=='E'||c=='I'||c=='O'||c=='U');
+if(lowercasevowel||uppercasevowel)
+{
+printf("%c is vowelof"c");
+}
+else
+{
+printf("is constant");
+}
+}
