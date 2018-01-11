42e393f308266ae5326f486e84c89515ac92a12a
ishwarya97 <30829605+ishwarya97@users.noreply.github.com>
2018-01-11 16:43:24 +0530

    Create count.c

---
+++ count.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,count=0;
+scanf("%d"&i);
+printf("the digit number");
+a=101;
+while(i!=10)
+{
+count++;
+}
+printf("the count",&a);
+}
+}
