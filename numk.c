6f2b98422a925852a302902ffb370c6e9c13a4b5
ishwarya97 <30829605+ishwarya97@users.noreply.github.com>
2018-01-11 16:46:10 +0530

    Create numk.c

---
+++ numk.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int N,k;
+int array N[10]={0,1,2,3};
+int array K[10]={5,6,7,8};
+scanf("%d%d',&N&k);
+printf("sum of the array number");
+add(sum)
+{
+sum=N+K;
+}
+printf(" ",&sum);
+}
+}
+
+
