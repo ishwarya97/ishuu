ad3258231fb9f9d77ff08e26ce764991e906a134
ishwarya97 <30829605+ishwarya97@users.noreply.github.com>
2018-01-19 15:56:10 +0530

    Create alpha.c

---
+++ alpha.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void  main()
+{
+char c;
+printf("enter a character");
+scanf("%d",&c);
+if(c='a'&&||c='z'&&||c='A'&&||c='Z')
+{
+printf("%c is alphabet");
+}
+else
+{
+printf("%c is not alphabet');
+}
+}
