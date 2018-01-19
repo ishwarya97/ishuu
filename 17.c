ed8f7c1170ef9bce47d232b995cf0da282c05fcd
ishwarya97 <30829605+ishwarya97@users.noreply.github.com>
2018-01-19 16:32:32 +0530

    Create 17.c

---
+++ 17.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int number,remaindernumber,result=0;
+printf("enter three digit number");
+int number=number;
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+}
