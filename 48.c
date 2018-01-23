0ccca8467bdeb313b5b65fa0e3f17eef38affe4d
ishwarya97 <30829605+ishwarya97@users.noreply.github.com>
2018-01-23 16:13:13 +0530

    Create 48.c

---
+++ 48.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+int a=10,b=4,c=3,d=7,e=8,f;
+f=a+b+c+d+e;
+scanf("%d%d%d%d%d",&a,&b,&c,&d,&e);
+printf("sum of numbers",&f);
+if(f%5)
+{
+printf("enter the average numbers');
+}
+}
