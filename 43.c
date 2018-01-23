1be5b56b3d389817b5fa1d29dc3fe49d1919c1b2
ishwarya97 <30829605+ishwarya97@users.noreply.github.com>
2018-01-23 16:30:26 +0530

    Create 43.c

---
+++ 43.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+char str1[25],str2[50];
+int i=0,j=0;
+printf("the first string:");
+gets(str1);
+printf("the second string:");
+gets(str2);
+if(str1>str2)
+{
+printf("concatinate the two strings');
+}
+
+}
+
