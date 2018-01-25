9684e7ecdd9d1bcd6c312d4d931268a36b7cb44d
ishwarya97 <30829605+ishwarya97@users.noreply.github.com>
2018-01-25 16:20:01 +0530

    Create 55.c

---
+++ 55.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a=20,b=20;
+scanf("%d",&a,&b);
+printf("enter a number");
+if(20%2)
+{
+printf("enter a even number");
+}
+else
+{
+printf("enter a odd number");
+}
+}
