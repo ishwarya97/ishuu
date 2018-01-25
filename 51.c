865820a371f3b531495e14a2756d1bff377bd2ea
ishwarya97 <30829605+ishwarya97@users.noreply.github.com>
2018-01-25 15:54:34 +0530

    Create 51.c

---
+++ 51.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int number,digits;
+printf("enter a number");
+scanf("%d",&num);
+for(int i=0;i<n;i++)
+{
+number=number+digits;
+{
+printf("sum of n numbers\n");
+}
+}
+}
