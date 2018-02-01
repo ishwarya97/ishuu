1664b23bbe28659152816c113b72394c6221a1c8
ishwarya97 <30829605+ishwarya97@users.noreply.github.com>
2018-02-01 11:10:22 +0000

    Create 68.c

---
+++ 68.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+int a,b;
+printf("enter a number");
+scanf("%d",&a);
+b=a*2;
+printf("%d",b);
+if(b>1)
+{
+  printf("\nyes");
+}
+else
+{
+  printf("no");
+}
+}
+
