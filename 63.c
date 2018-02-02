80ab7e8043bef8fe5b397033144c151e813df4f0
ishwarya97 <30829605+ishwarya97@users.noreply.github.com>
2018-02-02 09:25:28 +0000

    Create 63.c

---
+++ 63.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+char s[200];
+int count=0,i,n;
+printf("enter the number"):
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+if(s[i]==1)
+{
+count++;
+printf("number of the given statement is:%d\n",count+1);
+}
+}
+}
