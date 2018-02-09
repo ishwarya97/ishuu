d9a0202184d4f62f1727ee9f78d93979aed122ac
ishwarya97 <30829605+ishwarya97@users.noreply.github.com>
2018-02-09 09:21:34 +0000

    Create 78.c

---
+++ 78.c
@@ -0,0 +1,18 @@
+
+#include<stdio.h>
+void main()
+{
+int a,b;
+printf("enter a number");
+scanf("%d",&a);
+b=a*13;
+printf("%d",b);
+if(b>1)
+{
+  printf("\nyes");
+}
+else
+{
+  printf("no");
+}
+}
