7cde6b8ddb71941e5b0d5d5fc55043337a69c749
ishwarya97 <30829605+ishwarya97@users.noreply.github.com>
2018-02-09 08:40:41 +0000

    Create ps1.c

---
+++ ps1.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+void main()
+{
+  int a,n;
+  printf("enter a number");
+  scanf("%d",&a);
+switch(n)
+{
+case1:printf("monday");
+      break;
+case2:
+     printf("tuesday");
+      break;
+case3:
+     printf("wednesday");
+      break;
+case4:
+     printf("thursday");
+     break;
+case5:
+     printf("friday");
+     break;
+case6:
+     printf("saturday");
+default:printf("it is holiday");
+break;
+}
+}
