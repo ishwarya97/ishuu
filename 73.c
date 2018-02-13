a861f6b4eabd032a2c674b2b506b328d378a8981
ishwarya97 <30829605+ishwarya97@users.noreply.github.com>
2018-02-13 08:46:07 +0000

    Create 73.c

---
+++ 73.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+  int n,l=5,r=9;
+  printf("enter the number ");
+  scanf("%d",&n);
+  n=l%r;
+  if(l%r)
+  {
+    printf("yes");
+  }
+  else
+  {
+    printf("no");
+  }
+  }
