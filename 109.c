c29f56d6f1659ad162550589066b82bf17a2f67f
ishwarya97 <30829605+ishwarya97@users.noreply.github.com>
2018-02-22 16:08:03 +0530

    Create 109.c

---
+++ 109.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+
+    int a=10,n;
+    printf("enter a  10 number/n");
+    scanf("%d",&a);
+    if(n>=10)
+    {
+        printf("enter the numbers");
+        scanf("%d",&n);
+    }
+}
+
+
