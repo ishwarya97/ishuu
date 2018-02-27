424a23e281a9dc53076a6f731f2f4dde72ddf2bb
ishwarya97 <30829605+ishwarya97@users.noreply.github.com>
2018-02-27 16:17:15 +0530

    Create 76.c

---
+++ 76.c
@@ -0,0 +1,46 @@
+
+#include<stdio.h>
+
+int main(void)
+
+{
+
+int n,i,flag=0;
+printf("enter the number");
+
+scanf("%d",&n);
+
+for(i=2;i<=n/2;i++)
+
+{
+
+if(n%i==0)
+
+{
+
+flag=1;
+
+break;
+
+}
+
+}
+
+if(flag==0)
+
+{
+
+printf("\n no");
+
+}
+
+else
+
+{
+
+printf("\n yes");
+
+}
+
+return 0;
+}
